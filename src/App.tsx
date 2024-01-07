509a8861e9c953e05f7cb9ab36b0f006727bfdca
amirabas <abasaarafi@outlook.com>
2024-01-06 20:56:53 -0800

    context_childern_provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from "react"
-import User from './components/User'
-import { UserNameContext } from "./context/UsernameContext"
+import User from "./components/User"
+import CreateContextProvider from "./context/UsernameContext"
 
 function App() {
 
-  const [userName, setUsername] = useState < string > ('amir abas')
   
   return (
-   <UserNameContext.Provider
-   value={userName}
-   >
-      <div>
-        <User />
-      </div>
-   </UserNameContext.Provider>
+  <CreateContextProvider>
+    <div>
+      <User />
+    </div>
+  </CreateContextProvider>
   )
 }
 
--- src/context/UsernameContext.tsx
+++ src/context/UsernameContext.tsx
@@ -1,7 +1,31 @@
-import { createContext } from "react"
+import { createContext,useState } from "react"
 
+// store for send data
 const UserNameContext = createContext()
 
 export {
     UserNameContext
 }
+
+type Pro = {
+    title?: string;
+    children:
+    | JSX.Element
+    | JSX.Element[]
+    | string
+    | string[];
+};
+
+function CreateContextProvider({ childern }: Pro){
+
+    const [userName, setUsername] = useState < string > ('amir abas')
+
+    
+    return (
+        <UserNameContext.Provider value={userName}>
+            {childern}
+        </UserNameContext.Provider>
+    )
+}
+
+export default CreateContextProvider
